dd4c4a9e5307685ee24ce5da1a3d5ef6fd1e8680
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-06-07 10:26:59 +0400

    update css/html /Jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,14 +7,25 @@ $(document).ready(function () {
         $(this).children().removeClass("fa-angle-down");
         $(this).children().addClass("fa-angle-up");
     });
+    if ($("#head-nav").length) {
+        $(window).scroll(function (e) {
+            let scroll = $(window).scrollTop();
+            if (scroll > 5) {
+                $('#head-nav').addClass("sticky-menu");
+            } else {
+                $('#head-nav').removeClass("sticky-menu");
+            }
+
+        });
+    }
 
     $('.owl-carousel').owlCarousel({
         stagePadding: 50,
         loop: true,
         merge: true,
         items: 1,
-        autoWidth:true,
-        margin:1,
+        autoWidth: true,
+        margin: 40,
         // autoplay: true,
         autoplayTimeout: 5000,
     })
