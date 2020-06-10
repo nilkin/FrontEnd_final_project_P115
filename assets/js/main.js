9f2869b286ac5bf9f285ddd5955c3aabff071b0f
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-06-10 21:58:42 +0400

    add new page correct footer in index html add new sass /css for sections seperate  common classes to another path add new colors make second type of footer and navbar css and corrected buges in html create breadcrumb-area,data-solutions

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,30 @@ $(document).ready(function () {
         });
     }
 
+    $("#head-nav-second .navbar-menu li a").mouseenter(function () {
+        $(this).children().removeClass("fa-angle-up");
+        $(this).children().addClass("fa-angle-down");
+    });
+    $("#head-nav-second .navbar-menu li a").mouseleave(function () {
+        $(this).children().removeClass("fa-angle-down");
+        $(this).children().addClass("fa-angle-up");
+    });
+    if ($("#head-nav-second").length) {
+        $(window).scroll(function (e) {
+            let scroll = $(window).scrollTop();
+            if (scroll > 5) {
+                $('#head-nav-second').addClass("sticky-menu-second");
+                $("#head-nav-second .navbar .anada-brand img").attr("src", "/assets/img/logo.png")
+
+            } else {
+                $('#head-nav-second').removeClass("sticky-menu-second");
+                $("#head-nav-second .navbar .anada-brand img").attr("src", "/assets/img/logo-light.png")
+            }
+
+        });
+    }
+
+
     $('.owl-carousel').owlCarousel({
         stagePadding: 50,
         loop: true,
