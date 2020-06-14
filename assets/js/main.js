36e4c4303d467e50c5dccd28fe3ef345de99b3d4
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-06-15 01:50:08 +0400

    add side bar and left side bar also responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,9 +34,11 @@ $(document).ready(function () {
             let scroll = $(window).scrollTop();
             if (scroll > 380) {
                 $('#head-nav-second').addClass("sticky-menu-second");
-                $("#head-nav-second .navbar .anada-brand img").attr("src", "/assets/img/logo.png")
+                $("#head-nav-second .navbar .anada-brand img").attr("src", "/assets/img/logo.png");
 
-            } else {
+            }
+
+            else {
                 $('#head-nav-second').removeClass("sticky-menu-second");
                 $("#head-nav-second .navbar .anada-brand img").attr("src", "/assets/img/logo-light.png")
             }
@@ -44,6 +46,19 @@ $(document).ready(function () {
         });
     }
 
+    if ($("#mobile-menu").length) {
+        $(window).scroll(function (e) {
+            let scroll = $(window).scrollTop();
+            if (scroll > 180) {
+                $('#mobile-menu').addClass("mob-menu-sticy");
+            }
+            else {
+                $('#mobile-menu').removeClass("mob-menu-sticy");
+
+            }
+
+        });
+    }
 
     $('.features-items').owlCarousel({
         stagePadding: 50,
@@ -52,8 +67,8 @@ $(document).ready(function () {
         items: 1,
         autoWidth: true,
         margin: 40,
-        // autoplay: true,
-        autoplayTimeout: 5000,
+        autoplay: true,
+        autoplayTimeout: 3000,
     });
 
     $('#services-items .services-carousel').owlCarousel({
@@ -89,13 +104,72 @@ $(document).ready(function () {
         wowAnimation();
     });
 
-    $('#contact-area .contact-items form button').click(function(e){
+    $('#contact-area .contact-items form button').click(function (e) {
         e.preventDefault();
         $('#contact-area .contact-items form #message').removeClass("d-none")
-       
+
     })
 
+    $("#head-nav .navbar-menu .attr-nav a i.flaticon-list").click(function (e) {
+        e.preventDefault();
+        console.log($("#side-bar"))
+        $("#side-bar").animate({ right: '-20px', opacity: '1' }, 100);
+        $("#side-bar .side-bar-items .close-bar .icon i").removeClass("fa-long-arrow-alt-right").addClass("fa-times");
 
 
+    });
+    $("#side-bar .side-bar-items .close-bar .icon i").click(function (e) {
+        e.preventDefault();
+        console.log($("#side-bar"));
+        $("#side-bar .side-bar-items .close-bar .icon i").removeClass("fa-times").addClass("fa-long-arrow-alt-right");
+
+        $("#side-bar").animate({ right: '-380px', opacity: '0' }, 100);
+
+
+    });
+    $("#head-nav-second .attribut .attr-nav a i.flaticon-list").click(function (e) {
+        e.preventDefault();
+        console.log($("#side-bar"))
+        $("#side-bar-second").animate({ right: '-20px', opacity: '1' }, 100);
+        $("#side-bar-second .side-bar-items .close-bar .icon i").removeClass("fa-long-arrow-alt-right").addClass("fa-times");
+
+
+    });
+    $("#mobile-menu .mob-items .attribute .attr-nav a i.flaticon-list").click(function (e) {
+        e.preventDefault();
+        console.log($("#side-bar"))
+        $("#side-bar").animate({ right: '57.7rem', opacity: '1' }, 100);
+        $("#side-bar .side-bar-items .close-bar .icon i").removeClass("fa-long-arrow-alt-right").addClass("fa-times");
+
+    });
+
+    $("#mobile-menu .mob-items .attribute .attr-nav a i.flaticon-list").click(function (e) {
+        e.preventDefault();
+        console.log($("#side-bar"))
+        $("#side-bar-second").animate({ right: '-20px', opacity: '1' }, 100);
+        $("#side-bar-second .side-bar-items .close-bar .icon i").removeClass("fa-long-arrow-alt-right").addClass("fa-times");
+
+    });
+
+    $("#side-bar-second .side-bar-items .close-bar .icon i").click(function (e) {
+        e.preventDefault();
+        console.log($("#side-bar"));
+        $("#side-bar-second .side-bar-items .close-bar .icon i").removeClass("fa-times").addClass("fa-long-arrow-alt-right");
+        $("#side-bar-second").animate({ right: '-380px', opacity: '0' }, 100);
+
+
+    });
+
+    $("#mobile-menu.mob-menu .mob-items .icon i.fa-bars").click(function (e) {
+        e.preventDefault();
+        $("#mobile-menu #navbar-left-menu").slideToggle(100).removeClass("d-none");
+        $(this).toggleClass("fa-times");
+    });
+    $("#mobile-menu #navbar-left-menu .navbar-menu .nav-item .nav-link").click(function (e) {
+        e.preventDefault();
+        $(this).next().slideToggle("slow").removeClass("d-none");
+        console.log($(this).next());
+
+    });
 });
 
